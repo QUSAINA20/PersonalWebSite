4d063c8cf6e30483c3c81ca34fcacbe722dfa700
QUSAINA20 <91684341+QUSAINA20@users.noreply.github.com>
2023-06-22 19:43:35 +0300

    edit sidbar

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -51,7 +51,7 @@
                 <a class="nav-link text-white {{ $activePage == 'client' ? ' active bg-gradient-primary' : '' }}  "
                     href="{{ route('clients.index') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">work</i>
+                        <i class="material-icons opacity-10">person</i>
                     </div>
                     <span class="nav-link-text ms-1">Clients</span>
                 </a>
